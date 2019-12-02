8bfec95f4b489c790cc93bb3a46798a5133e73ea
carpanla <carpanla@gmail.com>
2019-12-02 15:46:46 +0100

    add html and start js

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,5 +1,16 @@
 'use strict';
 
-console.log('>> Ready :)');
+const input = document.querySelector('#search-input');
+const button = document.querySelector('#search-button');
+const urlBase = 'http://api.tvmaze.com/search/shows?q=';
 
+function getInfo(){
+  //console.log('holi');
+  const name = input.value.toLowerCase();
+  fetch(urlBase + name)
+    .then(response => response.json())
+    .then(data => displaySerie(data));
+
+}
+button.addEventListener('click', getInfo);
 //# sourceMappingURL=main.js.map
